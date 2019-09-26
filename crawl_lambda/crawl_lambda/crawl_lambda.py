c563fe022f0f0e7cc10aa56e0986314f3a410791
songbyeongjin <song4489z@naver.com>
2019-09-27 00:13:44 +0900

    NATE_NEWS_LAMBDA_REFACTORING

--- crawl_lambda/crawl_lambda/crawl_lambda.py
+++ crawl_lambda/crawl_lambda/crawl_lambda.py
@@ -3,36 +3,8 @@ import requests
 import os
 import psycopg2
 import datetime
-import time
-import re
-
-#NEWS CLASS
-class News:
-    def __init__(
-        self,
-        id,
-        title,
-        content,
-        press,
-        writer,
-        date,
-        url,
-        portal,
-        created_at,
-        updated_at):
-
-        #initilaze
-        self.id = id
-        self.title = title
-        self.content = content
-        self.press = press
-        self.writer = writer
-        self.date = date
-        self.url = url
-        self.portal = portal
-        self.created_at = created_at
-        self.updated_at = updated_at
         
+
 #定数定義
 NOT_END_STR = "[MORE...]"
 
@@ -40,13 +12,8 @@ CONTENT_MAX_LEN = 200
 NEWS_LEN = 5
 CURRENT_YEAR = "2019"#<!年が経つと要変更
 NATE ="nate"
-
 #dateパラメーターが現在日・時間より遅く設定されると現在日・時間に繋がるため、2099年99月99日に設定する
 NATE_URL = "https://news.nate.com/rank/interest?sc=all&p=day&date=20999999"
-DAUM_URL = "https://media.daum.net/ranking/popular/"
-#naver HTML DOM構想の問題で、全年齢のRANKING NEWSが表示されないため、30代のデータを収集
-NAVER_URL = "https://news.naver.com/main/ranking/popularDay.nhn?rankingType=age&subType=30"
-
 
 def handler(event, context):
     response = requests.get(NATE_URL)
@@ -61,6 +28,32 @@ def handler(event, context):
     #nate url取得
     nate_crawl_url = get_nate_url(NATE_URL)
 
+    #DB接続
+    db_conn = psycopg2.connect(
+        host     = os.environ['host'],
+        dbname   = os.environ['database'],
+        user     = os.environ['username'],
+        password = os.environ['password'],
+        port     = os.environ['port'])
+
+    cursor = db_conn.cursor()
+
+    #DELETE query作成
+    sql_delete = "delete from news where portal = 'nate'"
+    #新しいNEWSを挿入する前に全てのNATE NEWSを削除する
+    cursor.execute(sql_delete,())
+    db_conn.commit()
+
+    #現在IDの最大値を取得するquery作成
+    sql_get_id = "select max(id) from news"
+    #NEWS挿入時に使用するレコードのKEYであるID取得
+    cursor.execute(sql_get_id,())
+    max_id = cursor.fetchall()[0][0]
+    #レコードが0件の場合
+    if max_id is None:
+        max_id = 0
+    new_id = max_id + 1
+
     for index in range(0,NEWS_LEN):
         #create temp nate news instance
 
@@ -71,7 +64,6 @@ def handler(event, context):
         #html content取得
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        
         #DB挿入用レコード作成
         insert_title   = get_nate_title(soup)
         insert_content = get_nate_content(soup)
@@ -86,23 +78,21 @@ def handler(event, context):
 
         #INSERT query作成
         #reprメソッドを使うと自動的に「''」に囲まれるので「''」省略する
-        sql = f"INSERT INTO news (id, title, content, press, writer, date, url, portal, created_at, updated_at)\n"
-        sql = sql + f"VALUES(default, {repr(insert_title)} ,'{insert_content}', '{insert_press}', '{insert_wrtier}', '{insert_date}'::timestamptz,"
-        sql = sql + f"{repr(insert_url)}, '{insert_portal}', '{insert_create_time}'::timestamptz, '{insert_updated_time}'::timestamptz);"
 
-        #create nate news
-        db_conn = psycopg2.connect(
-            host     = os.environ['host'],
-            dbname   = os.environ['database'],
-            user     = os.environ['username'],
-            password = os.environ['password'],
-            port     = os.environ['port'])
+        sql_insert = f"INSERT INTO news (id, title, content, press, writer, date, url, portal, created_at, updated_at)\n"
+        sql_insert = sql_insert + f"VALUES ({new_id}, {repr(insert_title)} ,'{insert_content}', '{insert_press}', '{insert_wrtier}', '{insert_date}'::timestamptz,"
+        sql_insert = sql_insert + f"{repr(insert_url)}, '{insert_portal}', '{insert_create_time}'::timestamptz, '{insert_updated_time}'::timestamptz);"
         
-        cursor = db_conn.cursor()
-        cursor.execute(sql,())
+        #create nate news
+        cursor.execute(sql_insert,())
         db_conn.commit()
-        cursor.close()
-        db_conn.close()
+
+        #次のNEWSの挿入時のため増加させる
+        new_id = new_id + 1
+
+    #DB処理終了
+    cursor.close()
+    db_conn.close()
 
 
 def get_nate_url(nate_url):
@@ -160,7 +150,8 @@ def get_nate_date(soup):
     date = soup.select('.firstDate em')
     temp_date = date[0].text
 
-    temp_date = temp_date[:9]
-    nate_crawl_date = datetime.datetime.strptime(temp_date, "%y-%m-%d")
+    #文字列のうち、yyyy-mm-dd形式のみ抽出
+    temp_date = temp_date[:10]
+    nate_crawl_date = datetime.datetime.strptime(temp_date, "%Y-%m-%d")
 
     return nate_crawl_date
