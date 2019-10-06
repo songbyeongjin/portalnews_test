85845172ae2430edc4a1fb77835747d1c30dba1a
songbyeongjin <song4489z@naver.com>
2019-10-06 14:00:44 +0900

    daum_lambda移行完了

--- crawl_lambda_daum/crawl_lambda_daum/crawl_lambda_daum.py
+++ crawl_lambda_daum/crawl_lambda_daum/crawl_lambda_daum.py
@@ -14,10 +14,6 @@ DAUM ="daum"
 DAUM_URL = "https://media.daum.net/ranking/popular/"
 
 def handler(event, context):
-    response = requests.get(DAUM)
-    parsedHtml = response.text
-    soup = BeautifulSoup(parsedHtml, 'html.parser')
-    
     ###################################################DAUM#############################################
     daum_news = []
 
@@ -49,23 +45,27 @@ def handler(event, context):
     new_id = max_id + 1
 
     #get 記事URL and 取材社名
-    urlCor = get_daum_url_cor(DAUM_URL)
+    response = requests.get(DAUM_URL)
+    parsedHtml = response.text
+    soup = BeautifulSoup(parsedHtml, 'html.parser')
+    urlCor = get_daum_url_cor(soup)
     daum_crwal_url = urlCor[0]
     daum_crwal_cor = urlCor[1]
 
     for index in range(0, NEWS_LEN):
-        crawled_daum_url = daum_crwal_url[index]
-        response = requests.get(crawled_daum_url)
+        crawled_daum_url = daum_crwal_url[index].get('href')
         
-        #html content取得
-        soup = BeautifulSoup(response.text, 'html.parser')
+        response = requests.get(crawled_daum_url)
+        parsedHtml = response.text
+        soup = BeautifulSoup(parsedHtml, 'html.parser')
+
 
         #DB挿入用レコード作成
         insert_title   = get_daum_title(soup)
         insert_content = get_daum_content(soup)
-        insert_press   = daum_crwal_cor[index]
+        insert_press   = daum_crwal_cor[index].text
         insert_wrtier  = "writer"
-        insert_url     = daum_crwal_url[index]#!<urlは予め取得しておいた値を使用する
+        insert_url     = daum_crwal_url[index].get('href')#!<urlは予め取得しておいた値を使用する
         insert_portal  = DAUM
         insert_date    = get_daum_date(soup)
         now_date = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
@@ -79,9 +79,7 @@ def handler(event, context):
         sql_insert = sql_insert + f"VALUES ({new_id}, '{insert_title}' ,'{insert_content}', '{insert_press}', '{insert_wrtier}', '{insert_date}'::timestamptz,"
         sql_insert = sql_insert + f"{repr(insert_url)}, '{insert_portal}', '{insert_create_time}'::timestamptz, '{insert_updated_time}'::timestamptz);"
         
-        print(insert_content)
 
-        print(sql_insert)
         #create daum news
         cursor.execute(sql_insert,())
         db_conn.commit()
@@ -95,27 +93,26 @@ def handler(event, context):
 
 #指定したURLから記事をcrwalし、必要な要素を抽出する
 #return urlリスト,corsリスト
-def get_daum_url_cor(daum_url):
+def get_daum_url_cor(soup):
     urlCor = []
     daum_crwal_url = []
     daum_crwal_cor = []
 
-    response = requests.get(daum_url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    daum_crwal_url = soup.select(".ranking_list li .ranking_headline a")
-    urlCor.append(daum_crwal_url.text)
+    daum_crwal_url = soup.select(".list_news2 li a")
+    urlCor.append(daum_crwal_url)
     daum_crwal_cor = soup.select(".info_news")
-    urlCor.append(daum_crwal_cor.text)
+    urlCor.append(daum_crwal_cor)
+    
+    return urlCor
 
 def get_daum_title(soup):
     daum_crwal_title = soup.select(".tit_view")
-    return daum_crwal_title.text
+    return daum_crwal_title[0].text
 
 def get_daum_content(soup):
     temp_contents = soup.select("#harmonyContainer p")
-    trimminged_content = " ".join(temp_contents.text)
-    daum_crwal_content = trimminged_content[0..CONTENT_MAX_LEN] + NOT_END_STR
+    trimminged_content = " ".join(temp_contents[0].text)
+    daum_crwal_content = trimminged_content[0:CONTENT_MAX_LEN] + NOT_END_STR
     return daum_crwal_content
 
 def get_daum_date(soup):
@@ -127,5 +124,5 @@ def get_daum_date(soup):
     #daum記事のdateが取得できない場合はデフォルトとして、当日の日付を利用する
     if daum_crwal_date == "":
         return datetime.datetime.now
-    else
+    else:
         return datetime.datetime.strptime(temp_date, "%Y-%m-%d")
